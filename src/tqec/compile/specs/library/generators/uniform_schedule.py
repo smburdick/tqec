4008e291e0a09caedee1702a437cb67b0467f679
Sam Burdick <samuelmburdick@gmail.com>
2026-04-08 10:53:36 -0400

    More diag

--- src/tqec/compile/specs/library/generators/uniform_schedule.py
+++ src/tqec/compile/specs/library/generators/uniform_schedule.py
@@ -1,6 +1,10 @@
+from typing import Literal
+
 from tqec.compile.specs.library.generators.utils import PlaquetteMapper
 from tqec.plaquette.compilation.base import PlaquetteCompiler
+from tqec.plaquette.rpng.rpng import RPNGDescription
 from tqec.plaquette.rpng.translators.base import RPNGTranslator
+from tqec.utils.enums import Basis, Orientation
 
 
 class UniformScheduleConventionGenerator:
@@ -13,3 +17,13 @@ class UniformScheduleConventionGenerator:
 
         """
         self._mapper = PlaquetteMapper(translator, compiler)
+
+    def get_uniform_rpng_descriptions(
+        self,
+        is_reversed: bool,
+        reset: Basis | None = None,
+        measurement: Basis | None = None,
+        reset_and_measured_indices: tuple[Literal[0, 1, 2, 3], ...] = (0, 1, 2, 3),
+    ) -> dict[Basis, dict[Orientation, RPNGDescription]]:
+        """Docstring."""
+        pass
--- src/tqec/compile/specs/library/uniform_schedule.py
+++ src/tqec/compile/specs/library/uniform_schedule.py
@@ -1,12 +1,39 @@
-from tqec.compile.specs.base import CubeBuilder
+import functools
+from typing import override
+
+from tqec.compile.blocks.block import Block
+from tqec.compile.specs.base import CubeBuilder, CubeSpec
 from tqec.compile.specs.library.generators.uniform_schedule import (
     UniformScheduleConventionGenerator,
 )
 from tqec.plaquette.compilation.base import PlaquetteCompiler
 from tqec.plaquette.rpng.translators.base import RPNGTranslator
+from tqec.utils.scale import LinearFunction
 
 
 class UniformScheduleCubeBuilder(CubeBuilder):
     def __init__(self, compiler: PlaquetteCompiler, translator: RPNGTranslator):
         """Implement uniform scheduling."""
         self._generator = UniformScheduleConventionGenerator(translator, compiler)
+
+    @override
+    def __call__(self, spec: CubeSpec, block_temporal_height: LinearFunction) -> Block:
+        """Instantiate a :class:`.Block` instance implementing the provided ``spec``."""
+        return self._call_impl(spec, block_temporal_height)
+
+    @functools.cache
+    def _call_impl(self, spec: CubeSpec, block_temporal_height: LinearFunction) -> Block:
+        pass
+        # kind = spec.kind
+        # if isinstance(kind, Port):
+        #     raise TQECError("Cannot build a block for a Port.")
+        # elif isinstance(kind, YHalfCube):
+        #     raise NotImplementedError("Y cube is not implemented.")
+        # template, pgen = self._get_template_and_plaquettes_generator(spec)
+        # return _get_block(
+        #     z_basis=kind.z,
+        #     has_spatial_junction_in_timeslice=spec.has_spatial_up_or_down_pipe_in_timeslice,
+        #     template=template,
+        #     plaquettes_generator=pgen,
+        #     repetitions=block_temporal_height,
+        # )
